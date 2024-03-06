a4726b8418687e83e5a05a3abf1c69908f831f19
Takahiro Tsuchiya <takahiro.tsuchiya@tri-ad.global>
2024-03-06 18:34:23 +0900

    02_creating_python_files_and_control_flow/01_challengeの回答

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,7 +1,13 @@
 def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp = int(input('Please enter the temperature: '))
+    temp_unit = input('Please enter f for Fahrenheit or c for Celsius: ')
+
+    if (temp_unit == 'f'):
+        temp = (temp - 32) * 5/9
+    elif (temp_unit == 'c'):
+        temp = (temp * 9/5) + 32
 
     return temp
 
