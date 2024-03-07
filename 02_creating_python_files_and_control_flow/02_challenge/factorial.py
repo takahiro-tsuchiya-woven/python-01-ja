8749d7e5a74e4c1fd878d9d4a2a765f71410532a
Takahiro Tsuchiya <takahiro.tsuchiya@tri-ad.global>
2024-03-07 14:05:29 +0900

    02_creating_python_files_and_control_flow/02_challengeの回答

--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,10 +1,18 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
+    number = int(input('Please enter an integer grater than or equal to 1: '))
 
     # result変数を編集し、最終的な計算結果を保存します
     result = 0
+
+    if (number >= 0):
+        result = 1
+        while (number > 0):
+            result = result * number
+            number -= 1
+    else:
+        print("Please enter an integer greater than or equal to 1")
 
     return result
 
