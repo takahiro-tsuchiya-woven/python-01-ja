ec9cb06efa2b5741b6d6a3ab799a6dca367a2be4
Takahiro Tsuchiya <takahiro.tsuchiya@tri-ad.global>
2024-03-07 14:49:56 +0900

    02_creating_python_files_and_control_flow/03_challengeの回答

--- 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
+++ 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
@@ -1,1 +1,31 @@
 # ここにコードを書いてください
+# def main():
+#     # 文字列の入力
+#     input_string = input('Enter a string: ')
+
+#     # 母音のカウント
+#     num_vowels = 0
+
+#     for x in input_string:
+#         if (x == 'a' or x == 'i' or x == 'u' or x == 'e' or x == 'o'):
+#             num_vowels += 1
+
+#     print('Number of vowels:' + str(num_vowels))
+#     # return('Number of vowels:' + str(num_vowels))
+
+
+# if  __name__ == "__main__":
+#     main()
+
+
+# 文字列の入力
+input_string = input('Enter a string: ')
+
+# 母音のカウント
+num_vowels = 0
+
+for x in input_string:
+    if (x == 'a' or x == 'i' or x == 'u' or x == 'e' or x == 'o'):
+        num_vowels += 1
+
+print('Number of vowels:' + str(num_vowels))
