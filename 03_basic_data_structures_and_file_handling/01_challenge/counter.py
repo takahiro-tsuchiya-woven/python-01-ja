5169687877347a6bdf59d69984c1d5460321c3f0
Takahiro Tsuchiya <takahiro.tsuchiya@tri-ad.global>
2024-03-13 19:18:01 +0900

    03_basic_data_structures_and_file_handling/01_challengeの回答

--- 03_basic_data_structures_and_file_handling/01_challenge/counter.py
+++ 03_basic_data_structures_and_file_handling/01_challenge/counter.py
@@ -14,6 +14,14 @@ def counter():
     ]
 
     # ここにコードを書いてください
+    for fruit in fruits:
+        if fruit in occurrences:
+            occurrences[fruit] += 1
+        else:
+            occurrences[fruit] = 1
+
+    for x, y in occurrences.items():
+        print(str(x) + ': ' + str(y))
 
     return occurrences
 
