df1185fe4a2ecaa8576580734c2c7598cecec01e
Takahiro Tsuchiya <takahiro.tsuchiya@tri-ad.global>
2024-03-14 14:21:44 +0900

    03_basic_data_structures_and_file_handling/02_challengeの回答を修正

--- 03_basic_data_structures_and_file_handling/02_challenge/rps.py
+++ 03_basic_data_structures_and_file_handling/02_challenge/rps.py
@@ -1,9 +1,9 @@
 import random
 
-choice_of_a = random.randint(1, 3)
-choice_of_b = random.randint(1, 3)
+# choice_of_a = random.randint(1, 3)
+# choice_of_b = random.randint(1, 3)
 
-print(choice_of_a, choice_of_b)
+# print(choice_of_a, choice_of_b)
 
 # 1:Rock, 2:Paper, 3:Scissors
 
@@ -13,13 +13,30 @@ print(choice_of_a, choice_of_b)
 # b 2 b - a
 # b 3 a b -
 
-if choice_of_a == choice_of_b:
-    print("Draw")
-elif (choice_of_a - choice_of_b) == 1:
-    print("A win")
-elif (choice_of_a - choice_of_b) == 2:
-    print("B win")
-elif (choice_of_b - choice_of_a) == 1:
-    print("B win")    
+# if choice_of_a == choice_of_b:
+#     print("Draw")
+# elif (choice_of_a - choice_of_b) == 1:
+#     print("A win")
+# elif (choice_of_a - choice_of_b) == 2:
+#     print("B win")
+# elif (choice_of_b - choice_of_a) == 1:
+#     print("B win")    
+# else:
+#     print("A win")
+
+hands = ['rock', 'paper', 'scissors']
+
+choice_of_a1 = random.choice(hands)
+choice_of_b1 = random.choice(hands)
+
+if choice_of_a1 == choice_of_b1:
+    result = 'Draw'
+elif (choice_of_a1 == 'rock' and choice_of_b1 == 'scissors') or (choice_of_a1 == 'scissors' and choice_of_b1 == 'paper') or (choice_of_a1 == 'paper' and choice_of_b1 == 'rock'):
+    result = 'A1 win'
 else:
-    print("A win")
+    result = 'B1 win'
+
+print('A1:' + choice_of_a1 + ', B1:' + choice_of_b1 + ', result:' + result)
+
+
+
