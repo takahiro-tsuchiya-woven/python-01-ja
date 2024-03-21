52074e340e6d74fd0e8f92946dffddf2cf42d18b
Takahiro Tsuchiya <takahiro.tsuchiya@tri-ad.global>
2024-03-21 15:33:36 +0900

    03_basic_data_structures_and_file_handling/03_challengeの回答

--- 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
+++ 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
@@ -6,7 +6,36 @@ def ls_command(directory):
     # 無効なディレクトリパスについて考える必要はありません
 
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    # pass
+    files = os.listdir(directory)
+
+    result = {}
+
+    for file in files:
+        path, ext = os.path.splitext(file)
+
+        # 拡張子がない、フォルダの場合はスキップ
+        if ext == "":
+            continue
+
+        if ext in result.keys():
+            result[ext] += 1
+        elif ext not in result.keys():
+            result[ext] = 1
+    
+    if any(result):
+        result_sorted = sorted(result.items())
+        # print(type(result_sorted))
+        print("Summary in alphabetical order:")
+        
+        for result_sorted_print in result_sorted:
+            if result_sorted_print[1] > 1:
+                print(result_sorted_print[0] + ":" + str(result_sorted_print[1]) + " files")
+            elif result_sorted_print[1] == 1:
+                print(result_sorted_print[0] + ":" + str(result_sorted_print[1]) + " file")
+    else:
+        print("There is no file in the directory.")
+
 
 
 if __name__ == "__main__":
