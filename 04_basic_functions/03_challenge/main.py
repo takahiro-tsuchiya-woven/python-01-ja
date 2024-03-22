bbea8793815154f64b79dc170b57b27a348e600e
Takahiro Tsuchiya <takahiro.tsuchiya@tri-ad.global>
2024-03-22 16:21:47 +0900

    04_basic_functions/03_challengeの回答

--- 04_basic_functions/03_challenge/main.py
+++ 04_basic_functions/03_challenge/main.py
@@ -1,6 +1,17 @@
 def unique_substrings(input_string):
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    unique_list = []
+    input_string_length = len(input_string)
+
+    for i in range(input_string_length):
+        # print(i)
+        for n in range(input_string_length - i):
+            # print("n",n)
+            # print(input_string[i: i + n + 1])
+            if(input_string[i: i + n + 1] not in unique_list):
+                unique_list.append(input_string[i: i + n + 1])
+
+    return unique_list
 
 
 input_string = "banana"
